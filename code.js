fd10963394bf3b109b0c359f4fa296b0f3f4adcd
Ann <ann-egorova2000@yandex.ru>
2022-06-18 21:18:53 +0300

    initial commit

---
+++ code.js
@@ -0,0 +1,78 @@
+"use strict";
+
+// let number = 5.4;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "Ann",
+//     age: 30,
+//     isMarried: false
+// };
+
+// console.log(obj.isMarried);
+
+// let arr = ['plum.png', 'orange.jpg', 7, [], {}];
+// console.log(arr[2]);
+
+// const obj2 = {
+//     a: 5
+// };
+
+// obj2.a = 7;
+// obj2 = 1;
+// console.log(obj2.a);
+// console.log(obj2);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers  = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+//ИНТЕРПОЛЯЦИЯ
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = "Ann";
+
+// alert(`hi, ${user}`);
+
+// let incr = 10,
+//     decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr)
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 != '6');
+
+// const isChecked = false,
+//     isClose = false;
+
+// console.log(isChecked || !isClose);
